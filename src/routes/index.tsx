8d35efbb8ff41732df7968dcc64a3e08cb26c38b
FelicityIris <77635620+FelicityIris@users.noreply.github.com>
2026-06-30 23:08:03 +0530

    style: format via biome

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,5 +3,5 @@ import { createFileRoute } from "@tanstack/react-router";
 export const Route = createFileRoute("/")({ component: Home });
 
 function Home() {
-    return <h1>Welcome to Hostel Management Suite developed in Tanstack.</h1>;
+	return <h1>Welcome to Hostel Management Suite developed in Tanstack.</h1>;
 }
--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -4,5 +4,5 @@ import { LoginPage } from "../pages/LoginPage";
 export const Route = createFileRoute("/login")({ component: LoginRoute });
 
 function LoginRoute() {
-    return <LoginPage></LoginPage>
+	return <LoginPage></LoginPage>;
 }
